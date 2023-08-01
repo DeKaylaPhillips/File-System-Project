2ccaacf19f34b6adf78c2dc5a06780b395f06b2d
DeKayla Phillips <dphillips@8thlight.com>
2023-08-01 15:00:37 -0400

    Add functionality to read a file

--- src/file_system.py
+++ src/file_system.py
@@ -7,3 +7,11 @@ class FileSystem:
         with open(self.file_name, 'w') as self.file:
             new_file = self.file.name
         print(new_file)
+
+    def read_file(self):
+        try:
+            with open(self.file_name, 'r') as self.file:
+                content = self.file.read()
+            print(content)
+        except FileNotFoundError:
+            print(f'ERROR: File not found.')
