0d78af8bdaf4f62d5d484c731f5a7db7928440ac
DeKayla Phillips <philldek@oregonstate.edu>
2023-08-04 13:14:14 -0400

    Read From An Existing File (#2)
    * Add functionality to read a file
    
    * Add mock test to test the file system can read existing files
    
    Refactored test suite to implement helper mock functions
    
    * Refactor to invoke read function
    
    * Refactor read_file() method
    
    * Add test to read files & refactor test suite
    
    Added mock test to test that the file system can properly read the file data. Refactored test suite to use helper mock fns. Fixing casing for mock variables. Added mock csv data to use in mock tests.

--- src/file_system.py
+++ src/file_system.py
@@ -7,3 +7,8 @@ class FileSystem:
         with open(self.file_name, 'w') as self.file:
             new_file = self.file.name
         print(new_file)
+
+    def read_file(self):
+        with open(self.file_name, 'r') as self.file:
+            content = [line.rstrip('\n').split(',') for line in self.file]
+        print(content)
--- tests/test_fs.py
+++ tests/test_fs.py
@@ -1,12 +1,23 @@
 from src.file_system import FileSystem
 
 mock_file_path = 'mockFile.csv'
+mock_csv_data = 'Header1,Header2,Header3\nColumn1,Column2,Column3'
 fs = FileSystem(mock_file_path)
+mock_open = lambda mocker: mocker.patch('builtins.open', mocker.mock_open(read_data=mock_csv_data))
+mock_print = lambda mocker: mocker.patch('builtins.print')
 
 def test_mock_create_file(mocker):
-    mock_open_fn = mocker.patch('builtins.open', mocker.mock_open())
-    mock_print_fn = mocker.patch('builtins.print')
+    mock_open_fn = mock_open(mocker)
+    mock_print_fn = mock_print(mocker)
     mock_mode = 'w'
     fs.create_file()
     mock_open_fn.assert_called_once_with(mock_file_path, mock_mode)
     mock_print_fn.assert_called_once_with(fs.file.name)
+
+def test_mock_read_file(mocker):
+    mock_open_fn = mock_open(mocker)
+    mock_print_fn = mock_print(mocker)
+    mock_mode = 'r'
+    fs.read_file()
+    mock_open_fn.assert_called_once_with(mock_file_path, mock_mode)
+    mock_print_fn.assert_called_once_with([['Header1', 'Header2', 'Header3'], ['Column1', 'Column2', 'Column3']])
