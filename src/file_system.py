0b88715921ca343fcb80b7976f151eeae641b31a
DeKayla Phillips <dphillips@8thlight.com>
2023-08-07 13:34:44 -0400

    Refactor and add update method
    FS class refactored to now accept a validator module and a formatting module. Removed file instance method and references to it; replaced with local `file` variable when needed. Added headers_list parameter to create file, and refactored so implementation now knows to add formatted headers to the new file on creation. Added `update_file()` method to allow an Admin to add new data to a pre-existing file. Added private `__get_column_count` method to calculate the maximum amount of columns needed when writing data to the pre-existing file.

--- src/file_system.py
+++ src/file_system.py
@@ -1,14 +1,31 @@
-class FileSystem:
-    def __init__(self, file_descriptor):
+from src.validator import Validator
+class FileSystem: 
+    def __init__(self, file_descriptor, Validator, Formatter):
         self.file_name = file_descriptor
-        self.file = None
+        self.validate = Validator()
+        self.format = Formatter()
     
-    def create_file(self): 
-        with open(self.file_name, 'w') as self.file:
-            new_file = self.file.name
+    def create_file(self, headers_list): 
+        with open(self.file_name, 'w') as file:
+            new_file = file.name
+            headers = self.format.get_csv_formatted_headers(headers_list)
+            file.write(headers)
         print(new_file)
 
     def read_file(self):
-        with open(self.file_name, 'r') as self.file:
-            content = [line.rstrip('\n').split(',') for line in self.file]
+        with open(self.file_name, 'r') as file:
+            content = [line.rstrip('\n').split(',') for line in file]
         print(content)
+    
+    def update_file(self, data_list):
+        if self.validate.is_valid_data_input(self.__get_column_count(), data_list):
+            data = self.format.get_csv_formatted_data(data_list)
+            with open(self.file_name, 'a') as file:
+                for row in data:
+                    file.write(row)
+                print('File successfully updated.')
+
+    def __get_column_count(self):
+        with open(self.file_name, 'r') as file:
+            headers = file.readline().split(',')
+            return len(headers)
--- tests/test_fs.py
+++ tests/test_fs.py
@@ -1,23 +1,40 @@
 from src.file_system import FileSystem
+from src.validator import Validator
+from src.formatter import Formatter
 
 mock_file_path = 'mockFile.csv'
-mock_csv_data = 'Header1,Header2,Header3\nColumn1,Column2,Column3'
-fs = FileSystem(mock_file_path)
-mock_open = lambda mocker: mocker.patch('builtins.open', mocker.mock_open(read_data=mock_csv_data))
+mock_headers_list = ['Header1', 'Header2', 'Header3']
+mock_data_list = [['Column1','Column2','Column3']]
+mock_csv_content = 'Header1,Header2,Header3\nColumn1,Column2,Column3\n'
+fs = FileSystem(mock_file_path, Validator, Formatter)
+mock_open = lambda mocker: mocker.patch('builtins.open', mocker.mock_open(read_data=mock_csv_content))
 mock_print = lambda mocker: mocker.patch('builtins.print')
 
-def test_mock_create_file(mocker):
+def test_system_will_create_new_files_with_headers(mocker):
     mock_open_fn = mock_open(mocker)
+    mock_file = mock_open_fn.return_value
     mock_print_fn = mock_print(mocker)
     mock_mode = 'w'
-    fs.create_file()
+    fs.create_file(mock_headers_list)
     mock_open_fn.assert_called_once_with(mock_file_path, mock_mode)
-    mock_print_fn.assert_called_once_with(fs.file.name)
+    mock_print_fn.assert_called_once_with(mock_file.name)
+    mock_file.write.assert_called_once()
 
-def test_mock_read_file(mocker):
+def test_system_will_read_data_from_a_file(mocker):
     mock_open_fn = mock_open(mocker)
     mock_print_fn = mock_print(mocker)
     mock_mode = 'r'
     fs.read_file()
     mock_open_fn.assert_called_once_with(mock_file_path, mock_mode)
     mock_print_fn.assert_called_once_with([['Header1', 'Header2', 'Header3'], ['Column1', 'Column2', 'Column3']])
+
+def test_system_will_update_data_in_existing_files(mocker):
+    mock_open_fn = mock_open(mocker)
+    mock_file = mock_open_fn.return_value
+    mock_mode = 'a'
+    mock_csv_data_list = Formatter().get_csv_formatted_data(mock_data_list)
+    fs.update_file(mock_data_list)
+    mock_open_fn.assert_called_with(mock_file_path, mock_mode)
+    with mock_open_fn(mock_file_path, 'a') as mock_file:
+        for row in mock_csv_data_list:
+            mock_file.write.assert_called_with(row)
